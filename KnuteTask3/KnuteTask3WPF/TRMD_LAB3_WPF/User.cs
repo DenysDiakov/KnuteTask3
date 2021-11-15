b313da22f767d61bf0b513e6d4fb6c8d3f435b8b
DenysDiakov <47400573+DenysDiakov@users.noreply.github.com>
2021-11-15 17:40:30 +0200

    Add files via upload

---
+++ KnuteTask3/KnuteTask3WPF/TRMD_LAB3_WPF/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TRMD_LAB3_WPF
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Gender { get; set; }
+    }
+}
